2b232178855afcc6ab2cb48ccaef7407a3e8cde8
LawrenceShum <lawrencesom1104@gmail.com>
2020-11-07 14:21:56 +0800

    update

--- main.cpp
+++ main.cpp
@@ -9,10 +9,23 @@ using namespace DirectX::PackedVector;
 
 ostream& XM_CALLCONV operator << (ostream& os, FXMVECTOR v)
 {
-	XMFLOAT3 dest;
-	XMStoreFloat3(&dest, v);
+	XMFLOAT4 dest;
+	XMStoreFloat4(&dest, v);
 
-	os << "(" << dest.x << ", " << dest.y << ", " << dest.z << ")";
+	os << "(" << dest.x << ", " << dest.y << ", " << dest.z << ", " << dest.w << ")";
+	return os;
+}
+
+ostream& XM_CALLCONV operator << (ostream& os, FXMMATRIX m)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		os << XMVectorGetX(m.r[i]) << "\t";
+		os << XMVectorGetY(m.r[i]) << "\t";
+		os << XMVectorGetZ(m.r[i]) << "\t";
+		os << XMVectorGetW(m.r[i]);
+		os << endl;
+	}
 	return os;
 }
 
@@ -26,17 +39,28 @@ int main()
 		return 0;
 	}
 
-	XMVECTOR p = XMVectorZero();
-	XMVECTOR q = XMVectorSplatOne();
-	XMVECTOR u = XMVectorSet(1.0f, 2.0f, 3.0f, 0.0f);
-	XMVECTOR v = XMVectorReplicate(-2.0f);
-	XMVECTOR w = XMVectorSplatZ(u);
+	XMMATRIX A( 1.0f, 0.0f, 0.0f, 0.0f,
+				0.0f, 2.0f, 0.0f, 0.0f,
+				0.0f, 0.0f, 4.0f, 0.0f,
+				1.0f, 2.0f, 3.0f, 1.0f);
 
-	cout << "p = " << p << endl;
-	cout << "q = " << q << endl;
-	cout << "u = " << u << endl;
-	cout << "v = " << v << endl;
-	cout << "w = " << w << endl;
+	XMMATRIX B = XMMatrixIdentity();
+	XMMATRIX C = A * B;
+
+	XMMATRIX D = XMMatrixTranspose(A);
 	
+	XMVECTOR det = XMMatrixDeterminant(A);
+	XMMATRIX E = XMMatrixInverse(&det, A);
+
+	XMMATRIX F = A * E;
+
+	cout << "A = " << endl << A << endl;
+	cout << "B = " << endl << B << endl;
+	cout << "C = A * B = " << endl << C << endl;
+	cout << " D = transpose (A) = " << endl << D << endl;
+	cout << "det = determinant (A) = " << endl << det << endl << endl;
+	cout << "E = inverse(A) = " << endl << E << endl;
+	cout << "F = A * E = " << endl << F << endl;
+
 	return 0;
 }
